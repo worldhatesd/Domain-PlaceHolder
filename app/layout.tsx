33d440cce87d0817b3e8c62f37b59a0203178c85
WorldHatesD <40483048+worldhatesd@users.noreply.github.com>
2024-01-28 04:40:11 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: `${window.location.hostname}`,
+  title: `${document.location.hostname}`,
   description: 'Domain placeholder',
 }
 
